7f42524a952785b264f5a2130d0dbfd55f550947
lapcca <lapcca@163.com>
2025-03-29 22:53:11 +0800

    add listview and table view

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 slint::include_modules!();
+use std::rc::Rc;
+use slint::{ModelRc, StandardListViewItem, VecModel, SharedString};
 fn main() -> Result<(), slint::PlatformError> {
 
     let window = MainWindow::new()?;
+    // Create a VecModel and put it in an Rc.
+    let the_model : Rc<VecModel<StandardListViewItem>> =
+        Rc::new(VecModel::from(vec!["Hello".into(), "World".into()]));
+    // Convert it to a ModelRc.
+    let the_model_rc = ModelRc::from(the_model.clone());
+    // Pass the model to the ui: The generated set_the_model setter from the
+    // the_model property takes a ModelRc.
+    window.set_types(the_model_rc);
     window.run()
 }
